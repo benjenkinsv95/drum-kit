da79b2a0f97d5d33cdfd3ab50a297ebeebb79fde
benjenkinsv95 <benjenkinsv95@gmail.com>
2019-04-08 19:23:51 -0400

    Updates

---
+++ precache-manifest.4c1ce92c86149a8de9ee5c5d0461534c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/drum-kit/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "214658d2b2d57624db56e06b26942d1d",
+    "url": "/drum-kit/static/media/background.214658d2.jpg"
+  },
+  {
+    "revision": "7b6327eefb8c5630e0e5",
+    "url": "/drum-kit/static/js/runtime~main.3ae46f20.js"
+  },
+  {
+    "revision": "447f543c1290a5db5d90",
+    "url": "/drum-kit/static/js/main.4ca715ad.chunk.js"
+  },
+  {
+    "revision": "3bb596feb725fa2a3ea7",
+    "url": "/drum-kit/static/js/2.e0e8f203.chunk.js"
+  },
+  {
+    "revision": "447f543c1290a5db5d90",
+    "url": "/drum-kit/static/css/main.8fa93118.chunk.css"
+  },
+  {
+    "revision": "00481749a9fc17cc351b469ee565a18a",
+    "url": "/drum-kit/index.html"
+  }
+];
