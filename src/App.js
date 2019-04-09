1ef5485715cbba46c83ef6ea466e34677b2d5fc7
benjenkinsv95 <benjenkinsv95@gmail.com>
2019-04-09 09:28:59 -0400

    Making mobile friendly

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
         <div className="keys">
           <Row>
             {drums.map(({letter, sound, color}) =>
-              <Col key={letter} xs={4} xl="auto">
+              <Col key={letter} xs={4} xl="auto" align="center">
                 <Drum
                   letter={letter}
                   sound={sound}
